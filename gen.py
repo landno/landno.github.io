d889a19d3b2b5ed0e7b3a832d9dc899d4916e8b1
landno <273032298@qq.com>
2024-11-11 14:04:13 +0800

    something

---
+++ gen.py
@@ -0,0 +1,15 @@
+import os
+import json
+import sys
+
+input_title = sys.argv[1]
+
+
+if input_title[0] == 't':
+	pass
+if input_title[0] == 'w':
+	for i in os.listdir('json'):
+		js = json.load(open('json/'+i))
+		if js['volumeId'] == 'workbook':
+			if js['humanId'].split('-')[1] == input_title[1:]:
+				os.system('python3 1.py '+str(js['sectionId'])+' > workbook/'+input_title+'.html')
