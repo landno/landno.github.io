58f33920bd26dd4adca6012a9ea53594ea9b06a2
landno <273032298@qq.com>
2024-11-14 00:03:13 +0800

    something

--- n2.py
+++ n2.py
@@ -3,14 +3,15 @@ import json
 import sys
 from natsort import natsorted
 from bs4 import BeautifulSoup
-
+scp ='''
+<script src="../acim.js"></script>
+'''
 bodyH='''
 <!DOCTYPE html>
 <head>
 <meta charset=UTF-8>
 <link rel="stylesheet" href="../style.css">
 <script src="../jquery-3.7.1.min.js"></script>
-<script src="../acim.js"></script>
 </head>
 <body>
 
@@ -26,7 +27,8 @@ for i in os.listdir('epub'):
 	with open('epub/'+i) as f:
 		r_lst.append(f.read())
 
-#print(bodyH)
+print(bodyH)
+
 js = json.load(open('json/'+num+'.json'))
 tit = ''
 if js['titleInfo'].get('title'):
@@ -34,58 +36,85 @@ if js['titleInfo'].get('title'):
 else:
 	tit = js['seoTitle'].split('|')[0]
 print('<div id="title">%s</div>'%tit)
+print(js['bodyHtml'])
 tag = ''
 r_t_lst = []
 x_t_lst = []
 if js['volumeId'] == 'text':
 	tag = js['annotation']+'.'
-for i in r_lst:
-	#print(i)
-	soup = BeautifulSoup(i,'html.parser')
-	content = soup.text
-	for i in range(1,30):
-		r = content.find(tag+str(i))
-		if r>0:
-			r2 = content.find(tag+str(i+1))
-			if r2>0:
-				r_t_lst.append(content[r:r2])
-			else:
-				r_t_lst.append(content[r:])
+	for i in r_lst:
+		#print(i)
+		soup = BeautifulSoup(i,'html.parser')
+		content = soup.text
+		for i in range(1,30):
+			r = content.find(tag+str(i))
+			if r>0:
+				r2 = content.find(tag+str(i+1))
+				if r2>0:
+					r_t_lst.append(content[r:r2])
+				else:
+					r_t_lst.append(content[r:])
+	if int(num)<100:
+		num = '0'+num
+	for i in r_t_lst:
+		#j = i[len(tag):].find('.')
 
-for i in r_t_lst:
-	#j = i[len(tag):].find('.')
+		#first
+		k = i.find('2',len(tag)+3)
+		if k>0:
+			print('<div class="popup" tid="u%s">%s</div>'%(str(num)+'#'+i.split('.')[1]+':1',i[0:k].replace('\n','')))
+		else:
+			print('<div class="popup" tid="u%s">%s</div>'%(str(num)+'#'+i.split('.')[1]+':1',i[0:].replace('\n','')))
 
-	#first
-	k = i.find('2',len(tag)+3)
-	if k>0:
-		print(i[0:k].strip()+'here')
-	else:
-		print(i.strip()+'hrer')
-
-	#last
-	for k in range(2,30):
-		c1 = i.find(str(k),len(tag)+3)
-		c2 = i.find(str(k+1))
-		if c1>0 and c2>0:
-			print(i[c1:c2].strip())
-		elif c1>0 and c2<0:
-			print(i[c1:].strip())
-#print(r_t_lst)
-x_soup = BeautifulSoup(open('x_html/'+js['humanId']+'.html'),'html.parser')
+		#last
+		for k in range(2,30):
+			c1 = i.find(str(k),len(tag)+3)
+			c2 = i.find(str(k+1))
+			if c1>0 and c2>0:
+				t = i[c1:c2]
+				print('<div class="popup" tid="u%s">%s</div>'%(str(num)+'#'+i.split('.')[1]+':'+t.split(' ')[0],t[len(str(k))+1:].replace('\n','')))
+				#print(i[c1:c2].strip())
+			elif c1>0 and c2<0:
+				t = i[c1:]
+				print('<div class="popup" tid="u%s">%s</div>'%(str(num)+'#'+i.split('.')[1]+':'+t.split(' ')[0],t[len(str(k))+1:].replace('\n','')))
+				#print(i[c1:].strip())
+	#print(r_t_lst)
+	x_soup = BeautifulSoup(open('x_html/'+js['humanId']+'.html'),'html.parser')
 
 
-for i in x_soup.find_all('p'):
-	#print(i)
-	k = ['0','1','2','3','4','5','6','7','8','9']
-	if str(i)[3:4] in k:
-		x_t_lst.append(i.text)
-	else:
-		x_t_lst[len(x_t_lst)-1]=str(x_t_lst[len(x_t_lst)-1])+i.text
-		#print(str(i)[3:4]+' not num')
-	#print(i.text.strip())
+	for i in x_soup.find_all('p'):
+		#print(i)
+		k = ['0','1','2','3','4','5','6','7','8','9']
+		if str(i)[3:4] in k:
+			x_t_lst.append(i.text)
+		else:
+			x_t_lst[len(x_t_lst)-1]=str(x_t_lst[len(x_t_lst)-1])+i.text
+			#print(str(i)[3:4]+' not num')
+		#print(i.text.strip())
+	#print(x_t_lst)
 
-for i in x_t_lst:
-	for j in range(2,30):
-		print(j)
+
+
+
+
+	for i in x_t_lst:
+		#first
+		k = i.find('2',3)
+		if k>0:
+			#print('<div class="popup" tid="%s">%s</div>'%('u'+input_num+'#'+str(i[0])+':'+str(j+1),i[1][j].strip()+'。'))
+			print('<div class="popup" tid="d%s">%s</div>'%(str(num)+'#'+i.split('.')[0]+':1',i[0:k].replace('\n','')))
+		else:
+			print('<div class="popup" tid="d%s">%s</div>'%(str(num)+'#'+i.split('.')[0]+':1',i[0:].replace('\n','')))
+		for j in range(2,30):
+			k1 = i.find(str(j),3)
+			k2 = i.find(str(j+1),3)
+			if k1>0 and k2>0:
+				print('<div class="popup" tid="d%s">%s</div>'%(str(num)+'#'+i.split('.')[0]+':'+str(j),i[k1+len(str(j)):k2].replace('\n','')))
+			elif k1>0 and k2<0:
+				print('<div class="popup" tid="d%s">%s</div>'%(str(num)+'#'+i.split('.')[0]+':'+str(j),i[k1+len(str(j)):].replace('\n','')))
+
 #print(js['bodyHtml'])
-#print(bodyF)
+print('<div display="none" class="popup" id="utext"></div>')
+print('<div display="none" class="popup" id="dtext"></div>')
+print(scp)
+print(bodyF)
---
+++ n3g.py
@@ -0,0 +1,8 @@
+import os
+import json
+from natsort import natsorted
+for i in natsorted(os.listdir('json')):
+	for j in range(0,52):
+		if i.split('.')[0] == str(j):
+			js = json.load(open('json/'+i))
+			os.system('python3 n2.py '+str(j)+' > html/'+js['humanId']+'.html')
