35a5e7da3d0e86c49a57e0847c7012072c7038dc
landno <273032298@qq.com>
2024-11-14 10:44:21 +0800

    something

--- n3g.py
+++ n3g.py
@@ -2,7 +2,7 @@ import os
 import json
 from natsort import natsorted
 for i in natsorted(os.listdir('json')):
-	for j in range(0,85):
+	for j in range(90,100):
 		if i.split('.')[0] == str(j):
 			js = json.load(open('json/'+i))
 			print(j)
