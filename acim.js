27b1d3728434d73c2e1fe36e848f2aa5ad9b9ad8
landno <273032298@qq.com>
2024-11-15 03:50:01 +0800

    something

--- acim.js
+++ acim.js
@@ -1,6 +1,8 @@
 $("span").dblclick(function(){
 	$("span").css("text-decoration","none");
 	var pos = $(this).position()
+	var fontSize = $(this).css('font-size');
+	var lineHeight = Math.floor(parseInt(fontSize.replace('px','')) * 1.5);
 	$(this).css("text-decoration","underline");
 	let dnid = "d"+$(this).attr('data-sentence-id')
 	let dinsert_text = $("div").filter(function(){
@@ -11,10 +13,10 @@ $("span").dblclick(function(){
 		return $(this).attr('tid') == unid
 	}).first().text()
 	$("#dtext").text(dinsert_text)
-	$("#dtext").css({top:pos.top+$(this).height()+2,left:pos.left,position:"absolute",backgound:"grey"});
+	$("#dtext").css({top:pos.top-lineHeight-$('#dtext').height(),left:pos.left,position:"absolute",backgound:"grey"});
 	$("#dtext").show()
 	$("#utext").text(uinsert_text)
-	$("#utext").css({top:pos.top+$(this).height()+$("#dtext").height()+28,left:pos.left,position:"absolute",backgound:"grey"});
+	$("#utext").css({top:pos.top-$("#dtext").innerHeight()-lineHeight-$("#utext").innerHeight()+16,left:pos.left,position:"absolute",backgound:"grey"});
 	$("#utext").show()
 
 });
